39a7c9afd7a1aa1f79a28516447b42eb5f403b1e
sofi_flin <sofi_flin@sofi.flin.book>
2023-09-27 09:09:45 +0300

    Database approved

--- app/migrations/models.py
+++ app/migrations/models.py
@@ -9,6 +9,8 @@ class ApplicationForCalculation(models.Model):
     date_application_complete = models.DateField(blank=True, null=True)
     application_status = models.TextField()  # This field type is a guess.
     moderator = models.ForeignKey('Users', models.DO_NOTHING, related_name='applicationforcalculation_moderator_set', blank=True, null=True)
+    input_first_param = models.FloatField()
+    input_second_param = models.FloatField()
 
     class Meta:
         managed = False
@@ -16,26 +18,140 @@ class ApplicationForCalculation(models.Model):
 
 
 class ApplicationsCalculations(models.Model):
-    application = models.ForeignKey(ApplicationForCalculation, models.DO_NOTHING, db_column='application')
-    calculation = models.ForeignKey('CalculationTypes', models.DO_NOTHING, db_column='calculation')
+    application = models.OneToOneField(ApplicationForCalculation, models.DO_NOTHING, primary_key=True)  # The composite primary key (application_id, calculation_id) found, that is not supported. The first column is selected.
+    calculation = models.ForeignKey('CalculationTypes', models.DO_NOTHING)
+    result = models.FloatField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'applications_calculations'
+        unique_together = (('application', 'calculation'),)
 
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
 
 
 class CalculationTypes(models.Model):
     calculation_id = models.AutoField(primary_key=True)
     calculation_name = models.CharField(max_length=30)
     calculation_description = models.CharField(max_length=1500)
-    calculation_image_url = models.CharField(max_length=50, blank=True, null=True)
+    calculation_image_url = models.CharField(max_length=50, default='../static/images/base.png')
     calculation_status = models.TextField()  # This field type is a guess.
 
     class Meta:
         managed = False
         db_table = 'calculation_types'
 
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
 
 
 class Users(models.Model):
